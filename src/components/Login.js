c4f2793ac15dd00e5fecdb11248a37f1b6bf0dbf
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-06-18 08:57:12 -0700

    Login page with access to token and storage is complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,77 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 
 const Login = () => {
+  const [bubble, setBubble] = useState({
+    credentials: {
+      username:"Lambda",
+      password:"School"
+    }
+
+  });
+  const [errors, setErrors] = useState({
+    error:"",
+  })
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const login = (e) =>{
+    e.preventDefault();
+    if(bubble.credentials.username !== "Lambda" || bubble.credentials.password !== "School")
+    {
+      setErrors ({error:"Username or Password not valid"});
+    }
+    axios
+    .post('http://localhost:5000/api/login',bubble.credentials)
+    .then(res=>{
+      //console.log(res.data.payload);
+      localStorage.setItem("token", res.data.payload);
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
 
-  const error = "";
+  const error = errors.error;
   //replace with error state
+
+  const handleChange = (e) => {
+    console.log("e:", e.target.value);
+    setBubble({
+      credentials: {
+        ...bubble.credentials,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
         <h2>Build login form here</h2>
+      </div>
+      <div>
+        <form onSubmit={login}>
+          {/* <form onSubmit> */}
+          <input
+            type="text"
+            data-testid="username"
+            name="username"
+            value={bubble.credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            data-testid="password"
+            name="password"
+            value={bubble.credentials.password}
+            onChange={handleChange}
+          />
+          <p data-testid="errorMessage" className="error">
+            {error}
+          </p>
+          <button>Log in</button>
+        </form>
       </div>
 
       <p data-testid="errorMessage" className="error">{error}</p>
